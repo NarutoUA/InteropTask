0a9971c9a9a1acbab5aad99f21711e0beb482556
NarutoUA <narutomacs@gmail.com>
2017-01-25 14:23:32 +0200

    Remove credentials from Settings Editor

--- InteropTask/SettingsEditorVBNet/MainForm.Designer.vb
+++ InteropTask/SettingsEditorVBNet/MainForm.Designer.vb
@@ -28,14 +28,7 @@ Partial Class frmMain
         Me.lblConnType = New System.Windows.Forms.Label()
         Me.lblConnStr = New System.Windows.Forms.Label()
         Me.txtConnStr = New System.Windows.Forms.TextBox()
-        Me.grbCreds = New System.Windows.Forms.GroupBox()
-        Me.txtCredPassword = New System.Windows.Forms.TextBox()
-        Me.txtCredUser = New System.Windows.Forms.TextBox()
-        Me.lblCredPassword = New System.Windows.Forms.Label()
-        Me.lblCredUsername = New System.Windows.Forms.Label()
-        Me.chkSaveCreds = New System.Windows.Forms.CheckBox()
         Me.btnSave = New System.Windows.Forms.Button()
-        Me.grbCreds.SuspendLayout()
         Me.SuspendLayout()
         '
         'lblDbProvType
@@ -70,7 +63,7 @@ Partial Class frmMain
         'lblConnType
         '
         Me.lblConnType.AutoSize = True
-        Me.lblConnType.Location = New System.Drawing.Point(16, 57)
+        Me.lblConnType.Location = New System.Drawing.Point(13, 57)
         Me.lblConnType.Name = "lblConnType"
         Me.lblConnType.Size = New System.Drawing.Size(87, 13)
         Me.lblConnType.TabIndex = 3
@@ -79,7 +72,7 @@ Partial Class frmMain
         'lblConnStr
         '
         Me.lblConnStr.AutoSize = True
-        Me.lblConnStr.Location = New System.Drawing.Point(16, 230)
+        Me.lblConnStr.Location = New System.Drawing.Point(13, 97)
         Me.lblConnStr.Name = "lblConnStr"
         Me.lblConnStr.Size = New System.Drawing.Size(92, 13)
         Me.lblConnStr.TabIndex = 4
@@ -87,74 +80,14 @@ Partial Class frmMain
         '
         'txtConnStr
         '
-        Me.txtConnStr.Location = New System.Drawing.Point(12, 246)
+        Me.txtConnStr.Location = New System.Drawing.Point(16, 113)
         Me.txtConnStr.Name = "txtConnStr"
         Me.txtConnStr.Size = New System.Drawing.Size(354, 20)
         Me.txtConnStr.TabIndex = 5
         '
-        'grbCreds
-        '
-        Me.grbCreds.Controls.Add(Me.txtCredPassword)
-        Me.grbCreds.Controls.Add(Me.txtCredUser)
-        Me.grbCreds.Controls.Add(Me.lblCredPassword)
-        Me.grbCreds.Controls.Add(Me.lblCredUsername)
-        Me.grbCreds.Controls.Add(Me.chkSaveCreds)
-        Me.grbCreds.Enabled = False
-        Me.grbCreds.Location = New System.Drawing.Point(19, 105)
-        Me.grbCreds.Name = "grbCreds"
-        Me.grbCreds.Size = New System.Drawing.Size(354, 122)
-        Me.grbCreds.TabIndex = 6
-        Me.grbCreds.TabStop = False
-        Me.grbCreds.Text = "Credentials:"
-        '
-        'txtCredPassword
-        '
-        Me.txtCredPassword.Enabled = False
-        Me.txtCredPassword.Location = New System.Drawing.Point(9, 71)
-        Me.txtCredPassword.Name = "txtCredPassword"
-        Me.txtCredPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
-        Me.txtCredPassword.Size = New System.Drawing.Size(152, 20)
-        Me.txtCredPassword.TabIndex = 4
-        '
-        'txtCredUser
-        '
-        Me.txtCredUser.Enabled = False
-        Me.txtCredUser.Location = New System.Drawing.Point(9, 32)
-        Me.txtCredUser.Name = "txtCredUser"
-        Me.txtCredUser.Size = New System.Drawing.Size(154, 20)
-        Me.txtCredUser.TabIndex = 3
-        '
-        'lblCredPassword
-        '
-        Me.lblCredPassword.AutoSize = True
-        Me.lblCredPassword.Location = New System.Drawing.Point(6, 55)
-        Me.lblCredPassword.Name = "lblCredPassword"
-        Me.lblCredPassword.Size = New System.Drawing.Size(56, 13)
-        Me.lblCredPassword.TabIndex = 2
-        Me.lblCredPassword.Text = "Password:"
-        '
-        'lblCredUsername
-        '
-        Me.lblCredUsername.AutoSize = True
-        Me.lblCredUsername.Location = New System.Drawing.Point(6, 16)
-        Me.lblCredUsername.Name = "lblCredUsername"
-        Me.lblCredUsername.Size = New System.Drawing.Size(58, 13)
-        Me.lblCredUsername.TabIndex = 1
-        Me.lblCredUsername.Text = "Username:"
-        '
-        'chkSaveCreds
-        '
-        Me.chkSaveCreds.AutoSize = True
-        Me.chkSaveCreds.Location = New System.Drawing.Point(9, 97)
-        Me.chkSaveCreds.Name = "chkSaveCreds"
-        Me.chkSaveCreds.Size = New System.Drawing.Size(105, 17)
-        Me.chkSaveCreds.TabIndex = 0
-        Me.chkSaveCreds.Text = "Save credentials"
-        Me.chkSaveCreds.UseVisualStyleBackColor = True
-        '
         'btnSave
         '
-        Me.btnSave.Location = New System.Drawing.Point(295, 272)
+        Me.btnSave.Location = New System.Drawing.Point(295, 139)
         Me.btnSave.Name = "btnSave"
         Me.btnSave.Size = New System.Drawing.Size(75, 23)
         Me.btnSave.TabIndex = 7
@@ -165,9 +98,8 @@ Partial Class frmMain
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(382, 303)
+        Me.ClientSize = New System.Drawing.Size(382, 174)
         Me.Controls.Add(Me.btnSave)
-        Me.Controls.Add(Me.grbCreds)
         Me.Controls.Add(Me.txtConnStr)
         Me.Controls.Add(Me.lblConnStr)
         Me.Controls.Add(Me.lblConnType)
@@ -179,8 +111,6 @@ Partial Class frmMain
         Me.Name = "frmMain"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "InteropTask Settings Editor"
-        Me.grbCreds.ResumeLayout(False)
-        Me.grbCreds.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -191,12 +121,6 @@ Partial Class frmMain
     Friend WithEvents lblConnType As System.Windows.Forms.Label
     Friend WithEvents lblConnStr As System.Windows.Forms.Label
     Friend WithEvents txtConnStr As System.Windows.Forms.TextBox
-    Friend WithEvents grbCreds As System.Windows.Forms.GroupBox
-    Friend WithEvents txtCredPassword As System.Windows.Forms.TextBox
-    Friend WithEvents txtCredUser As System.Windows.Forms.TextBox
-    Friend WithEvents lblCredPassword As System.Windows.Forms.Label
-    Friend WithEvents lblCredUsername As System.Windows.Forms.Label
-    Friend WithEvents chkSaveCreds As System.Windows.Forms.CheckBox
     Friend WithEvents btnSave As System.Windows.Forms.Button
 
 End Class
--- InteropTask/SettingsEditorVBNet/MainForm.vb
+++ InteropTask/SettingsEditorVBNet/MainForm.vb
@@ -1,8 +1,6 @@
 ﻿Imports Microsoft.Win32
 
 Public Class frmMain
-    Private Declare Sub Encrypt Lib "InteropCryptCppDll" (ByRef szText As String)
-    Private Declare Sub Decrypt Lib "InteropCryptCppDll" (ByRef szText As String)
     Private Const m_strAppSubkey As String = "InteropTask\"
     Private Const m_strAppTitle As String = "Interop Settings Editor"
 
@@ -11,11 +9,9 @@ Public Class frmMain
         Dim objRegKey As RegistryKey
         Dim lProviderType As Long
         Dim lConnectionType As Long
-        Dim lSaveCreds As Long
         Dim strConnStr As String
-        Dim strPassword As String
 
-        objRegKey = Registry.CurrentUser.OpenSubKey("Software\" & m_strAppSubkey, True)
+        objRegKey = Registry.CurrentUser.OpenSubKey("SOFTWARE\" & m_strAppSubkey, True)
 
         If objRegKey Is Nothing Then
             Dim tempKey As RegistryKey
@@ -27,23 +23,7 @@ Public Class frmMain
 
         lProviderType = objRegKey.GetValue(My.Resources.Registry.regProvType, 0)
         lConnectionType = objRegKey.GetValue(My.Resources.Registry.regConnType, 0)
-        lSaveCreds = objRegKey.GetValue(My.Resources.Registry.regSaveCreds, 0)
         strConnStr = objRegKey.GetValue(My.Resources.Registry.regConnStr, "")
-
-        grbCreds.Enabled = lConnectionType
-
-        If lSaveCreds <> 0 Then
-            chkSaveCreds.Checked = True
-            txtCredUser.Enabled = True
-            txtCredPassword.Enabled = True
-            txtCredUser.Text = objRegKey.GetValue(My.Resources.Registry.regUsername, "")
-            strPassword = objRegKey.GetValue(My.Resources.Registry.regPassword, "")
-
-            If strPassword <> "" Then
-                Decrypt(strPassword)
-                txtCredPassword.Text = strPassword
-            End If
-        End If
 
         cmbConnType.SelectedIndex = lConnectionType
         cmbDbProvType.SelectedIndex = lProviderType
@@ -75,21 +55,6 @@ Public Class frmMain
         objRegKey.SetValue(My.Resources.Registry.regProvType, cmbDbProvType.SelectedIndex)
         objRegKey.SetValue(My.Resources.Registry.regConnType, cmbConnType.SelectedIndex)
         objRegKey.SetValue(My.Resources.Registry.regConnStr, txtConnStr.Text)
-        objRegKey.SetValue(My.Resources.Registry.regSaveCreds, If(chkSaveCreds.Checked = True, 1, 0))
-
-        If (chkSaveCreds.Checked = True) Then
-            objRegKey.SetValue(My.Resources.Registry.regUsername, txtCredUser.Text)
-
-            strPassword = txtCredPassword.Text
-            If strPassword <> "" Then
-                Encrypt(strPassword)
-            End If
-
-            objRegKey.SetValue(My.Resources.Registry.regPassword, strPassword)
-        Else
-            objRegKey.DeleteValue(My.Resources.Registry.regUsername, False)
-            objRegKey.DeleteValue(My.Resources.Registry.regPassword, False)
-        End If
 
         objRegKey.Close()
 
@@ -97,16 +62,4 @@ Public Class frmMain
 
     End Sub
 
-    Private Sub chkSaveCreds_CheckedChanged(sender As Object, e As EventArgs) Handles chkSaveCreds.CheckedChanged
-
-        txtCredUser.Enabled = chkSaveCreds.Checked
-        txtCredPassword.Enabled = chkSaveCreds.Checked
-
-    End Sub
-
-    Private Sub cmbConnType_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbConnType.SelectedIndexChanged
-
-        grbCreds.Enabled = cmbConnType.SelectedIndex
-
-    End Sub
 End Class
